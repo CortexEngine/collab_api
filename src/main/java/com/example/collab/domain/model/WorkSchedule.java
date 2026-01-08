b0ad192fe8a04925b0e17df3f20614ff6feb6534
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-07 21:59:58 -0300

    implementando workschedule, que guarda as informações da escala de trabalho, um agrupador de worktime

--- src/main/java/com/example/collab/domain/model/WorkSchedule.java
+++ src/main/java/com/example/collab/domain/model/WorkSchedule.java
@@ -1,8 +1,28 @@
 package com.example.collab.domain.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class WorkSchedule {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Getter
+  @Setter
+  private String description;
+
+  @Getter
+  @Setter
+  private Integer workDaysPerWeek;
+
+  @Getter
+  @Setter
+  private Integer restDaysPerWeek;
+
 }
+
