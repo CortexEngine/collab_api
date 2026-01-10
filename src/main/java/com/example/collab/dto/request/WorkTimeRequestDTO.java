8f681c7039d0207586e8dce34f928daaf69596b9
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-10 19:37:54 -0300

    aplicando anotação Data no DTO para caso seja necessaria Getters e Setters

--- src/main/java/com/example/collab/dto/request/WorkTimeRequestDTO.java
+++ src/main/java/com/example/collab/dto/request/WorkTimeRequestDTO.java
@@ -6,7 +6,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+@Data
 public class WorkTimeRequestDTO {
 
   @NotBlank
