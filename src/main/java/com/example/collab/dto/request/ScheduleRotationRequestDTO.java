3f9291d6802dc58210ddb64ae7e5ee17bca2d0d2
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-10 16:52:08 -0300

    criando campos para o DTO de request de schedule rotation

--- src/main/java/com/example/collab/dto/request/ScheduleRotationRequestDTO.java
+++ src/main/java/com/example/collab/dto/request/ScheduleRotationRequestDTO.java
@@ -1,5 +1,29 @@
 package com.example.collab.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+
+@Data
 public class ScheduleRotationRequestDTO {
+
+  @NotBlank
+  @NotNull
+  @PositiveOrZero
+  private Integer workSchedule;
+
+  @NotBlank
+  @NotNull
+  @PositiveOrZero
+  private Integer workTime;
+
+  @NotBlank
+  @NotNull
+  @Min(1)
+  @Max(7)
+  private Integer dayOfWeek;
 
 }
