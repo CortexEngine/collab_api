46c2d3c31a91928252d9a7e6fb67b96d642b9962
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-10 19:26:35 -0300

    criando os campos para o DTO de request de Work Time

--- src/main/java/com/example/collab/dto/request/WorkTimeRequestDTO.java
+++ src/main/java/com/example/collab/dto/request/WorkTimeRequestDTO.java
@@ -1,5 +1,43 @@
 package com.example.collab.dto.request;
 
+import java.time.LocalTime;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class WorkTimeRequestDTO {
+
+  @NotBlank
+  @NotNull
+  private String description;
+
+  @NotNull
+  @NotBlank
+  private boolean isActive;
+
+  @NotBlank
+  @NotNull
+  private LocalTime initialTime;
+
+  @NotBlank
+  @NotNull
+  private LocalTime endTime;
+
+  @NotBlank
+  @NotNull
+  private LocalTime initialBreakTime;
+
+  @NotBlank
+  @NotNull
+  private LocalTime endBreakTime;
+
+  @NotBlank
+  @NotNull
+  private boolean requiresPunch;
+
+  @NotBlank
+  @NotNull
+  private boolean autoGeneratePunches;
+
 
 }
