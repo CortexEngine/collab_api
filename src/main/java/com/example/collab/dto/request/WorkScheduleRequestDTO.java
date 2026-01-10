e3c2b95f0e8a3bf4e30ce659ba69d68c757d12ff
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-10 19:35:53 -0300

    criando campos para o DTO de request do Work Schedule

--- src/main/java/com/example/collab/dto/request/WorkScheduleRequestDTO.java
+++ src/main/java/com/example/collab/dto/request/WorkScheduleRequestDTO.java
@@ -1,5 +1,32 @@
 package com.example.collab.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class WorkScheduleRequestDTO {
+
+  @NotBlank
+  @NotNull
+  private String description;
+
+  @NotNull
+  @NotBlank
+  @Min(1)
+  @Max(7)
+  private Integer workDaysPerWeek;
+
+  @NotNull
+  @NotBlank
+  @Min(1)
+  @Max(7)
+  private Integer restDaysPerWeek;
+
+  @NotNull
+  @NotBlank
+  private boolean isActive;
 
 }
