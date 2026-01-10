d6c7f0fa1f7b8c30b6b48d9d7c7b7e7806f74c80
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-10 19:25:04 -0300

    ajustando não é request é response

--- src/main/java/com/example/collab/dto/response/WorkTimeResponseDTO.java
+++ src/main/java/com/example/collab/dto/response/WorkTimeResponseDTO.java
@@ -2,41 +2,31 @@ package com.example.collab.dto.response;
 
 import java.time.LocalTime;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.Data;
+
+@Data
 public class WorkTimeResponseDTO {
 
-  @NotBlank
-  @NotNull
   private String description;
 
-  @NotNull
-  @NotBlank
   private boolean isActive;
 
-  @NotBlank
-  @NotNull
+  @JsonFormat(pattern="HH:mm:ss")
   private LocalTime initialTime;
 
-  @NotBlank
-  @NotNull
+  @JsonFormat(pattern="HH:mm:ss")
   private LocalTime endTime;
 
-  @NotBlank
-  @NotNull
+  @JsonFormat(pattern="HH:mm:ss")
   private LocalTime initialBreakTime;
 
-  @NotBlank
-  @NotNull
+  @JsonFormat(pattern="HH:mm:ss")
   private LocalTime endBreakTime;
 
-  @NotBlank
-  @NotNull
   private boolean requiresPunch;
 
-  @NotBlank
-  @NotNull
   private boolean autoGeneratePunches;
 
 }
