fd55b0386e737a70914c695467ff7db79abbd475
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-11 20:50:46 -0300

    implementando o mapper corretamente para o Work Time (Horário)

--- src/main/java/com/example/collab/mapper/WorkTimeMapper.java
+++ src/main/java/com/example/collab/mapper/WorkTimeMapper.java
@@ -1,13 +1,42 @@
 package com.example.collab.mapper;
 
+import org.mapstruct.*;
+
 import com.example.collab.domain.model.WorkTime;
+import com.example.collab.dto.request.WorkTimeRequestDTO;
+import com.example.collab.dto.response.WorkTimeResponseDTO;
 
-public class WorkTimeMapper {
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface WorkTimeMapper {
 
+  @Mapping(target = "description", source = "description")
+  @Mapping(target = "isActive", source = "isActive")
+  @Mapping(target = "initialTime", source = "initialTime")
+  @Mapping(target = "endTime", source = "endTime")
+  @Mapping(target = "initialBreakTime", source = "initialBreakTime")
+  @Mapping(target = "endBreakTime", source = "endBreakTime")
+  @Mapping(target = "requiresPunch", source = "requiresPunch")
+  @Mapping(target = "autoGeneratePunches", source = "autoGeneratePunches")
   WorkTime toEntity (WorkTimeRequestDTO dto);
 
+  @Mapping(target = "description", source = "description")
+  @Mapping(target = "isActive", source = "isActive")
+  @Mapping(target = "initialTime", source = "initialTime")
+  @Mapping(target = "endTime", source = "endTime")
+  @Mapping(target = "initialBreakTime", source = "initialBreakTime")
+  @Mapping(target = "endBreakTime", source = "endBreakTime")
+  @Mapping(target = "requiresPunch", source = "requiresPunch")
+  @Mapping(target = "autoGeneratePunches", source = "autoGeneratePunches")
   void updateEntity (@MappingTarget WorkTime workTime, WorkTime dto);
 
+  @Mapping(target = "description", source = "description")
+  @Mapping(target = "isActive", source = "isActive")
+  @Mapping(target = "initialTime", source = "initialTime")
+  @Mapping(target = "endTime", source = "endTime")
+  @Mapping(target = "initialBreakTime", source = "initialBreakTime")
+  @Mapping(target = "endBreakTime", source = "endBreakTime")
+  @Mapping(target = "requiresPunch", source = "requiresPunch")
+  @Mapping(target = "autoGeneratePunches", source = "autoGeneratePunches")
   WorkTimeResponseDTO toResponse(WorkTime workTime);
 
 }
