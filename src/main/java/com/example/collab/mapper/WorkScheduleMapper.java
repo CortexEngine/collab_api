97f2b4276bb7314fccc81e6acf82f5c9e93c2be0
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-11 19:43:23 -0300

    corrigindo mapper de Work Schedule

--- src/main/java/com/example/collab/mapper/WorkScheduleMapper.java
+++ src/main/java/com/example/collab/mapper/WorkScheduleMapper.java
@@ -1,13 +1,32 @@
 package com.example.collab.mapper;
 
+import org.mapstruct.*;
+
 import com.example.collab.domain.model.WorkSchedule;
+import com.example.collab.dto.request.WorkScheduleRequestDTO;
+import com.example.collab.dto.response.WorkScheduleResponseDTO;
 
-public class WorkScheduleMapper {
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface WorkScheduleMapper {
 
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "description", source = "description")
+  @Mapping(target = "workDaysPerWeek", source = "workDaysPerWeek")
+  @Mapping(target = "restDaysPerWeek", source = "restDaysPerWeek")
+  @Mapping(target = "isActive", source = "isActive")
   WorkSchedule toEntity (WorkScheduleRequestDTO dto);
 
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "description", source = "description")
+  @Mapping(target = "workDaysPerWeek", source = "workDaysPerWeek")
+  @Mapping(target = "restDaysPerWeek", source = "restDaysPerWeek")
+  @Mapping(target = "isActive", source = "isActive")
   void updateEntity (@MappingTarget WorkSchedule workSchedule, WorkSchedule dto);
 
+  @Mapping(target = "description", source = "description")
+  @Mapping(target = "workDaysPerWeek", source = "workDaysPerWeek")
+  @Mapping(target = "restDaysPerWeek", source = "restDaysPerWeek")
+  @Mapping(target = "isActive", source = "isActive")
   WorkScheduleResponseDTO toResponse(WorkSchedule workSchedule);
   
 }
