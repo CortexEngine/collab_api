d6d9f8828e239a2946b79c13e88cf786f4dc91b8
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-10 21:57:12 -0300

    criando campos do DTO de resposta de Schedule Rotation

--- src/main/java/com/example/collab/dto/response/WorkScheduleResponseDTO.java
+++ src/main/java/com/example/collab/dto/response/WorkScheduleResponseDTO.java
@@ -1,5 +1,16 @@
 package com.example.collab.dto.response;
 
+import lombok.Data;
+
+@Data
 public class WorkScheduleResponseDTO {
+
+  private String description;
+
+  private Integer workDaysPerWeek;
+
+  private Integer restDaysPerWeek;
+
+  private boolean isActive;
 
 }
