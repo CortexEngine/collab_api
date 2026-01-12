dc818343b1c975b911a84110dc2148c2a4b5a034
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-12 20:52:09 -0300

    impementando repository para consultas no banco de Schedule Rotation

---
+++ src/main/java/com/example/collab/repository/ScheduleRotationRespository.java
@@ -0,0 +1,17 @@
+package com.example.collab.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.collab.domain.model.ScheduleRotation;
+
+public interface ScheduleRotationRespository extends JpaRepository<ScheduleRotation, Long> {
+
+  Optional<ScheduleRotation> findByWorkScheduleId(Long workScheduleId);
+
+  Optional<ScheduleRotation> findByWorkScheduleIdAndWorkTimeId(Long workScheduleId, Long workTimeId);
+
+  Optional<ScheduleRotation> findByWorkScheduleIdAndDayIndex(Long workScheduleId, Integer dayIndex);
+
+}
