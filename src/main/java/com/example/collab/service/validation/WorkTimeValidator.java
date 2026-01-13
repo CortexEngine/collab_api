210f365556207a775320615df196a2802b6caa41
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-13 20:48:04 -0300

    criando a chamada das funções de validação de work time

--- src/main/java/com/example/collab/service/validation/WorkTimeValidator.java
+++ src/main/java/com/example/collab/service/validation/WorkTimeValidator.java
@@ -1,5 +1,32 @@
 package com.example.collab.service.validation;
 
+import java.time.LocalTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.collab.repository.WorkTimeRepository;
+
+@Component
 public class WorkTimeValidator {
+
+  WorkTimeRepository workTimeRepository;
+
+  @Autowired
+  public WorkTimeValidator(WorkTimeRepository workTimeRepository) {
+
+    this.workTimeRepository = workTimeRepository;
+
+  }
+
+  public void validateTimeEndBeforeIntialTime(LocalTime initialTime, LocalTime endTime){}
+
+  public void validateSameWorkTimes(LocalTime initialTime, LocalTime endTime, LocalTime initialBreakTime, LocalTime endBreakTime) {}
+
+  public void validateInitialandEndWorkTimeLimit (LocalTime initialTime, LocalTime endTime){}
+
+  public void validateInitialAndEndBreakTimeLimit(LocalTime initialBreakTime, LocalTime endBreakTime) {}
+
+  public void validadeIsActiveWorkTime(Long id) {}
 
 }
