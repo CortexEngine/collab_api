4df7c02c8d7c13fcdf27c2f95a5ad0760abd27bc
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-13 21:12:39 -0300

    criando chamadas para funções de validação de work schedule

--- src/main/java/com/example/collab/service/validation/WorkScheduleValidator.java
+++ src/main/java/com/example/collab/service/validation/WorkScheduleValidator.java
@@ -1,5 +1,26 @@
 package com.example.collab.service.validation;
 
+import org.springframework.stereotype.Component;
+
+import com.example.collab.repository.WorkScheduleRepository;
+
+@Component
 public class WorkScheduleValidator {
+
+  WorkScheduleRepository workScheduleRepository;
+
+  public WorkScheduleValidator(WorkScheduleRepository workScheduleRepository) {
+
+    this.workScheduleRepository = workScheduleRepository;
+
+  }
+
+  public void validateManyDaysPerWeek(Integer workDaysPerWeek) {}
+
+  public void validateManyRestDaysPerWeek(Integer restDaysPerWeek) {}
+
+  public void validateManyDaysPerWeek(Integer workDaysPerWeek, Integer restDaysPerWeek) {}
+
+  public void validateIsActiveWorkSchedule(Long id) {}
 
 }
