dfe3e6e5a1815621f656330d731f2c62db0c07b4
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-21 20:44:58 -0300

    criando a base da classe que vai validar Schedule Rotation

--- src/main/java/com/example/collab/service/validation/ScheduleRotationValidator.java
+++ src/main/java/com/example/collab/service/validation/ScheduleRotationValidator.java
@@ -1,5 +1,19 @@
 package com.example.collab.service.validation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.collab.repository.ScheduleRotationRespository;
+@Component
 public class ScheduleRotationValidator {
+
+  ScheduleRotationRespository scheduleRotationRespository;
+
+  @Autowired
+  public ScheduleRotationValidator(ScheduleRotationRespository scheduleRotationRespository){
+
+    this.scheduleRotationRespository = scheduleRotationRespository;
+
+  }
 
 }
