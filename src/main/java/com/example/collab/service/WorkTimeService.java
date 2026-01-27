8acaeb2cda6d06531e3b343c4854165e1939796c
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-26 21:40:06 -0300

    Criando construtores para a classe service de Work Time.

--- src/main/java/com/example/collab/service/WorkTimeService.java
+++ src/main/java/com/example/collab/service/WorkTimeService.java
@@ -1,8 +1,30 @@
 package com.example.collab.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.collab.mapper.WorkTimeMapper;
+import com.example.collab.repository.WorkTimeRepository;
+import com.example.collab.service.validation.WorkTimeValidator;
 
 @Service
 public class WorkTimeService {
+
+  private WorkTimeRepository workTimeRepository;
+
+  private WorkTimeValidator workTimeValidator;
+
+  private WorkTimeMapper workTimeMapper;
+
+  @Autowired
+  public WorkTimeService(WorkTimeRepository workTimeRepository, WorkTimeValidator workTimeValidator, WorkTimeMapper workTimeMapper) {
+
+    this.workTimeRepository = workTimeRepository;
+
+    this.workTimeValidator = workTimeValidator;
+
+    this.workTimeMapper = workTimeMapper;
+    
+  }
 
 }
