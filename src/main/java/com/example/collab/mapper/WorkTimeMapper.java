d4fe9e410b322aeef16e357b05d21ea24fb9d3d7
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-26 21:12:23 -0300

    retirando codigo não e simplificando classe mapper

--- src/main/java/com/example/collab/mapper/WorkTimeMapper.java
+++ src/main/java/com/example/collab/mapper/WorkTimeMapper.java
@@ -10,34 +10,10 @@ import com.example.collab.dto.response.WorkTimeResponseDTO;
 public interface WorkTimeMapper {
 
   @Mapping(target = "id", ignore = true)
-  @Mapping(target = "description", source = "description")
-  @Mapping(target = "isActive", source = "isActive")
-  @Mapping(target = "initialTime", source = "initialTime")
-  @Mapping(target = "endTime", source = "endTime")
-  @Mapping(target = "initialBreakTime", source = "initialBreakTime")
-  @Mapping(target = "endBreakTime", source = "endBreakTime")
-  @Mapping(target = "requiresPunch", source = "requiresPunch")
-  @Mapping(target = "autoGeneratePunches", source = "autoGeneratePunches")
   WorkTime toEntity (WorkTimeRequestDTO dto);
 
-  @Mapping(target = "description", source = "description")
-  @Mapping(target = "isActive", source = "isActive")
-  @Mapping(target = "initialTime", source = "initialTime")
-  @Mapping(target = "endTime", source = "endTime")
-  @Mapping(target = "initialBreakTime", source = "initialBreakTime")
-  @Mapping(target = "endBreakTime", source = "endBreakTime")
-  @Mapping(target = "requiresPunch", source = "requiresPunch")
-  @Mapping(target = "autoGeneratePunches", source = "autoGeneratePunches")
   void updateEntity (@MappingTarget WorkTime workTime, WorkTime dto);
 
-  @Mapping(target = "description", source = "description")
-  @Mapping(target = "isActive", source = "isActive")
-  @Mapping(target = "initialTime", source = "initialTime")
-  @Mapping(target = "endTime", source = "endTime")
-  @Mapping(target = "initialBreakTime", source = "initialBreakTime")
-  @Mapping(target = "endBreakTime", source = "endBreakTime")
-  @Mapping(target = "requiresPunch", source = "requiresPunch")
-  @Mapping(target = "autoGeneratePunches", source = "autoGeneratePunches")
   WorkTimeResponseDTO toResponse(WorkTime workTime);
 
 }
