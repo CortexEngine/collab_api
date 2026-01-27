9a349a27177b40df88b9c225627d570e7ddef373
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-01-26 21:42:23 -0300

    criando classe service para schedule rotation

--- src/main/java/com/example/collab/service/ScheduleRotationService.java
+++ src/main/java/com/example/collab/service/ScheduleRotationService.java
@@ -1,8 +1,30 @@
 package com.example.collab.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.collab.mapper.ScheduleRotationMapper;
+import com.example.collab.repository.ScheduleRotationRepository;
+import com.example.collab.service.validation.ScheduleRotationValidator;
 
 @Service
 public class ScheduleRotationService {
+
+  private final ScheduleRotationRepository scheduleRotationRepository;
+
+  private final ScheduleRotationValidator scheduleRotationValidator;
+
+  private final ScheduleRotationMapper scheduleRotationMapper;
+
+  @Autowired
+  public ScheduleRotationService(ScheduleRotationRepository scheduleRotationRepository, ScheduleRotationValidator scheduleRotationValidator, ScheduleRotationMapper scheduleRotationMapper) {
+
+    this.scheduleRotationRepository = scheduleRotationRepository;
+
+    this.scheduleRotationValidator = scheduleRotationValidator;
+
+    this.scheduleRotationMapper = scheduleRotationMapper;
+
+  }
 
 }
