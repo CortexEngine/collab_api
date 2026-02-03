4d305adb23c34a86a417ed90e590d7093e24b26f
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-02-03 14:11:10 -0300

    Implementando serviço para criação de Schedule Rotation.

--- src/main/java/com/example/collab/service/ScheduleRotationService.java
+++ src/main/java/com/example/collab/service/ScheduleRotationService.java
@@ -3,6 +3,8 @@ package com.example.collab.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.collab.dto.request.ScheduleRotationRequestDTO;
+import com.example.collab.dto.response.ScheduleRotationResponseDTO;
 import com.example.collab.mapper.ScheduleRotationMapper;
 import com.example.collab.repository.ScheduleRotationRepository;
 import com.example.collab.service.validation.ScheduleRotationValidator;
@@ -24,6 +26,26 @@ public class ScheduleRotationService {
     this.scheduleRotationValidator = scheduleRotationValidator;
 
     this.scheduleRotationMapper = scheduleRotationMapper;
+
+  }
+
+  public ScheduleRotationResponseDTO createScheduleRotation(ScheduleRotationRequestDTO req) {
+
+    scheduleRotationValidator.validateDayIndexesAndWorkdaysSize(req.dayIndexs(), req.workdays());
+
+    scheduleRotationValidator.validateMaxDayIndexPerSchedule(req.workSchedule(), req.workdays());
+
+    scheduleRotationValidator.validateMinDayIndexPerSchedule(req.workSchedule(), req.workdays());
+
+    scheduleRotationValidator.validateTotalDaysMatchSchedule(req.workSchedule(), req.dayIndexs());
+
+    scheduleRotationValidator.validateDuplicateDayIndex(req.workSchedule(), req.dayIndexs());
+
+    var scheduleRotation = scheduleRotationMapper.toEntity(req);
+
+    var savedScheduleRotation = scheduleRotationRepository.save(scheduleRotation);
+
+    return scheduleRotationMapper.toResponse(savedScheduleRotation);
 
   }
 
