d7366942a4ce2b1546c7852c25d82574faf7a300
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-02-10 20:53:31 -0300

    implementando serviço de criação de work time

--- src/main/java/com/example/collab/service/WorkTimeService.java
+++ src/main/java/com/example/collab/service/WorkTimeService.java
@@ -3,6 +3,9 @@ package com.example.collab.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.collab.domain.model.WorkTime;
+import com.example.collab.dto.request.WorkTimeRequestDTO;
+import com.example.collab.dto.response.WorkTimeResponseDTO;
 import com.example.collab.mapper.WorkTimeMapper;
 import com.example.collab.repository.WorkTimeRepository;
 import com.example.collab.service.validation.WorkTimeValidator;
@@ -25,6 +28,26 @@ public class WorkTimeService {
 
     this.workTimeMapper = workTimeMapper;
     
+  }
+
+  public WorkTimeResponseDTO createWorkTime(WorkTimeRequestDTO req) {
+
+    workTimeValidator.validateTimeEndBeforeInitialTime(req.initialTime(), req.endTime(), req.isOvernight());
+
+    workTimeValidator.validateSameWorkTimes(req.initialTime(), req.endTime(), req.initialBreakTime(), req.endBreakTime());
+
+    workTimeValidator.validateInitialAndEndWorkTimeLimit(req.initialTime(), req.endTime(), req.isOvernight());
+
+    workTimeValidator.validateInitialAndEndBreakTimeLimit(req.initialBreakTime(), req.endBreakTime(), req.isOvernight());
+
+    workTimeValidator.validateRequireAndAutoPunchSameTime(req.requiresPunch(), req.autoGeneratePunches());
+
+    WorkTime workTime = workTimeMapper.toEntity(req);
+
+    WorkTime savedWorkTime = workTimeRepository.save(workTime);
+
+    return workTimeMapper.toResponse(savedWorkTime);
+
   }
 
 }
