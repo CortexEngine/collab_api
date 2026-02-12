36fcfb6d9576405430506eef6926110841878acc
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-02-11 21:45:43 -0300

    implementando nova query JPA para buscar Work Time ativos

--- src/main/java/com/example/collab/repository/WorkTimeRepository.java
+++ src/main/java/com/example/collab/repository/WorkTimeRepository.java
@@ -1,6 +1,7 @@
 package com.example.collab.repository;
 
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -24,5 +25,7 @@ public interface WorkTimeRepository extends JpaRepository<WorkTime, Long> {
   Optional<WorkTime> findByIdAndIsActive(Long id,Boolean isActive);
 
   Optional<WorkTime> findByDescription(String description);
+
+  List<WorkTime> findByIsActive(boolean isActive);
 
 }
