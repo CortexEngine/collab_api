97b27942c63fde335d52a430c50ca3396735fd51
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-03-03 15:53:13 -0300

    Ajustando query customizada

--- src/main/java/com/example/collab/repository/ScheduleRotationRepository.java
+++ src/main/java/com/example/collab/repository/ScheduleRotationRepository.java
@@ -3,6 +3,8 @@ package com.example.collab.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.collab.domain.model.ScheduleRotation;
 
@@ -12,9 +14,11 @@ public interface ScheduleRotationRepository extends JpaRepository<ScheduleRotati
 
   Optional<ScheduleRotation> findByWorkScheduleIdAndWorkTimeId(Long workScheduleId, Long workTimeId);
 
-  Optional<ScheduleRotation> findByWorkScheduleIdAndDayIndexs(Long workScheduleId, Integer dayIndexs);
+  @Query("SELECT sr FROM ScheduleRotation sr WHERE sr.workSchedule.id = :workScheduleId AND :dayIndex MEMBER OF sr.dayIndexs")
+  Optional<ScheduleRotation> findByWorkScheduleIdAndDayIndexs(@Param("workScheduleId") Long workScheduleId, @Param("dayIndex") Integer dayIndex);
 
-  Long countByworkScheduleIdAndWorkday(Long workScheduleId, Boolean workday);
+  @Query("SELECT COUNT(w) FROM ScheduleRotation sr JOIN sr.workdays w WHERE sr.workSchedule.id = :workScheduleId AND w = :workday")
+  Long countByworkScheduleIdAndWorkday(@Param("workScheduleId") Long workScheduleId, @Param("workday") Boolean workday);
 
   Long countByWorkScheduleId(Long workScheduleId);
 
