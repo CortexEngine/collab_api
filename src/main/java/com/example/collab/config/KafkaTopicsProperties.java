f5b445faac67f1040568c4263641aab42050826d
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-03-15 18:08:56 -0300

    aplicando padrão de codigo

--- src/main/java/com/example/collab/config/KafkaTopicsProperties.java
+++ src/main/java/com/example/collab/config/KafkaTopicsProperties.java
@@ -10,10 +10,13 @@ public class KafkaTopicsProperties {
     private final Topics topics = new Topics();
 
     public Topics getTopics() {
+
         return topics;
+
     }
 
     public static class Topics {
+
         private String collaboratorCreated;
         private String collaboratorUpdated;
         private String collaboratorDeleted;
@@ -22,51 +25,75 @@ public class KafkaTopicsProperties {
         private String departmentDeleted;
 
         public String getCollaboratorCreated() {
+
             return collaboratorCreated;
+
         }
 
         public void setCollaboratorCreated(String collaboratorCreated) {
+
             this.collaboratorCreated = collaboratorCreated;
+
         }
 
         public String getCollaboratorUpdated() {
+
             return collaboratorUpdated;
+
         }
 
         public void setCollaboratorUpdated(String collaboratorUpdated) {
+
             this.collaboratorUpdated = collaboratorUpdated;
+
         }
 
         public String getCollaboratorDeleted() {
+
             return collaboratorDeleted;
+
         }
 
         public void setCollaboratorDeleted(String collaboratorDeleted) {
+
             this.collaboratorDeleted = collaboratorDeleted;
+
         }
 
         public String getDepartmentCreated() {
+
             return departmentCreated;
+
         }
 
         public void setDepartmentCreated(String departmentCreated) {
+
             this.departmentCreated = departmentCreated;
+
         }
 
         public String getDepartmentUpdated() {
+
             return departmentUpdated;
+
         }
 
         public void setDepartmentUpdated(String departmentUpdated) {
+
             this.departmentUpdated = departmentUpdated;
+
         }
 
         public String getDepartmentDeleted() {
+
             return departmentDeleted;
+
         }
 
         public void setDepartmentDeleted(String departmentDeleted) {
+
             this.departmentDeleted = departmentDeleted;
+            
         }
     }
 }
