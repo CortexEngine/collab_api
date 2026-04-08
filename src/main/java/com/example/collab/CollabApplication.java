7f8de14fdae70e80b05775ed74c49a54d2a79838
Pedro de Sá <pedro.trabalhos@outlook.com>
2026-04-08 20:33:01 -0300

    agendando excecução do serviço de envio de mensagem para o kafka

--- src/main/java/com/example/collab/CollabApplication.java
+++ src/main/java/com/example/collab/CollabApplication.java
@@ -2,7 +2,9 @@ package com.example.collab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CollabApplication {
 
@@ -11,6 +13,7 @@ public class CollabApplication {
 		SpringApplication.run(CollabApplication.class, args);
 
 		System.err.println("Está rodando!");
+		
 	}
 
 }
